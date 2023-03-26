cf66da4796838d1f7a76ef2660346d88051bb542
Toy Rik <toyrik@gmail.com>
2023-03-26 10:44:24 +0300

    Отображение списка вакансий.

--- resources/views/vacancies/index.blade.php
+++ resources/views/vacancies/index.blade.php
@@ -44,74 +44,88 @@
             <!-- /.card-body -->
         </div>
 
-        <table class="table table-striped projects">
-            <thead>
-            <tr>
-                <th style="width: 1%">
-                    #
-                </th>
-                <th style="width: 25%">
-                    Компания
-                </th>
-                <th style="width: 25%">
-                    Название вакансии
-                </th>
-                <th style="width: 8%" class="text-center">
-                    Статус
-                </th>
-                <th style="width: 20%">
-                    Действия
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            @if($vacancies->count())
-                @foreach($vacancies as $vacancy)
-                <tr>
-                    <td>
-                        {{ $vacancy->id }}
-                    </td>
-                    <td>
-                        Название компании
-                    </td>
-                    <td>
-                        <a>
-                            {{ $vacancy->title }}
-                        </a>
-                        <br>
-                        <small>
-                            {{ $vacancy->created_at }}
-                        </small>
-                    </td>
-                    <td class="project-state">
-                        <span class="badge badge-success">Офер</span>
-                    </td>
-                    <td class="project-actions text-right">
-                        <a class="btn btn-primary btn-sm" href="{{ route('vacancies.show', [$vacancy->id]) }}">
-                            <i class="fas fa-folder">
-                            </i>
-                            Просмотреть
-                        </a>
-                        <a class="btn btn-info btn-sm" href="{{ route('vacancies.edit', [$vacancy->id]) }}">
-                            <i class="fas fa-pencil-alt">
-                            </i>
-                            Редактировать
-                        </a>
-                        <a class="btn btn-danger btn-sm" href="{{ route('vacancies.delete', [$vacancy->id]) }}">
-                            <i class="fas fa-trash">
-                            </i>
-                            Удалить
-                        </a>
-                    </td>
-                </tr>
-                </tbody>
-                @endforeach
-            @else
-                <tr>
-                    <td colspan="5">Ничего не найдено</td>
-                </tr>
-            </tbody>
-            @endif
-        </table>
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Вакансии</h3>
+
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body p-0">
+
+                <table class="table table-striped projects">
+                    <thead>
+                    <tr>
+                        <th style="width: 1%">
+                            #
+                        </th>
+                        <th style="width: 25%">
+                            Компания
+                        </th>
+                        <th style="width: 25%">
+                            Название вакансии
+                        </th>
+                        <th style="width: 8%" class="text-center">
+                            Статус
+                        </th>
+                        <th style="width: 30%">
+                            Действия
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @if($vacancies->count())
+                            @foreach($vacancies as $vacancy)
+                                <tr>
+                                    <td>
+                                        {{ $vacancy->id }}
+                                    </td>
+                                    <td>
+                                        Название компании
+                                    </td>
+                                    <td>
+                                        <a>
+                                            {{ $vacancy->title }}
+                                        </a>
+                                        <br>
+                                        <small>
+                                            {{ $vacancy->created_at }}
+                                        </small>
+                                    </td>
+                                    <td>
+                                        <span class="badge badge-success">Офер</span>
+                                    </td>
+                                    <td>
+                                        <a class="btn btn-primary btn-sm" href="{{ route('vacancies.show', [$vacancy->id]) }}">
+                                            <i class="fas fa-folder">
+                                            </i>
+                                            Просмотреть
+                                        </a>
+                                        <a class="btn btn-info btn-sm" href="{{ route('vacancies.edit', [$vacancy->id]) }}">
+                                            <i class="fas fa-pencil-alt">
+                                            </i>
+                                            Редактировать
+                                        </a>
+                                        <a class="btn btn-danger btn-sm" href="{{ route('vacancies.destroy', [$vacancy->id]) }}">
+                                            <i class="fas fa-trash">
+                                            </i>
+                                            Удалить
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.card-body -->
+        </div>
+
     </section>
 @endsection
