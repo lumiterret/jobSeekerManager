471a26dc9eee49575ef08f54bde86234b47b348f
Toy Rik <toyrik@gmail.com>
2023-03-26 16:52:28 +0300

    Сидер Компаний

--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -16,8 +16,13 @@ class EmployerFactory extends Factory
      */
     public function definition(): array
     {
+        $description = PHP_EOL . $this->faker->sentence(3) . PHP_EOL;
+        for($i = 0; $i <=3; $i++ ) {
+            $description .= PHP_EOL . $this->faker->paragraph(random_int(2, 6)) . PHP_EOL;
+        }
         return [
-            //
+            'title' => $this->faker->sentence(3),
+            'description' => $description,
         ];
     }
 }
--- database/seeders/EmployerSeeder.php
+++ database/seeders/EmployerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class EmployerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Employer::factory()->count(15)->create();
     }
 }
