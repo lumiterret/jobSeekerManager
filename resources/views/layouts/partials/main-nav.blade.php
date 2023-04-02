1dc2be46af836cb4df2dc5523d8626424b4d2871
Toy Rik <toyrik@gmail.com>
2023-04-02 14:53:12 +0300

    Правим навигацию

--- resources/views/layouts/partials/main-nav.blade.php
+++ resources/views/layouts/partials/main-nav.blade.php
@@ -6,7 +6,10 @@
 <div class="sidebar">
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-            <img src="{{ Vite::asset('resources/img/avatar.png') }}" class="img-circle elevation-2" alt="User Image">
+            <img
+                src="{{ Vite::asset('resources/img/avatar.png') }}"
+                class="img-circle elevation-2"
+                alt="User Image">
         </div>
         <div class="info">
             <a href="#" class="d-block">Вася Ветров</a>
@@ -14,40 +17,72 @@
     </div>
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <li class="nav-item">
-                <a href="{{ route('vacancies.index') }}" class="nav-link">
+            <li class="nav-item"> {{-- Меню вакансий --}}
+                <a href="#" class="nav-link">
                     <i class="nav-icon fas fa-business-time"></i>
                     <p>
                         Вакансии
+                        <i class="fas fa-angle-left right"></i>
                     </p>
                 </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('vacancies.index') }}">
+                            <i class="far fa-circle nav-icon"></i>
+                            Список
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('vacancies.create') }}">
+                            <i class="far fa-circle nav-icon"></i>
+                            Добавить
+                        </a>
+                    </li>
+                </ul>
             </li>
-            <li class="nav-item">
-                <a href="{{ route('employers.index') }}" class="nav-link">
+            <li class="nav-item"> {{-- Меню компаний --}}
+                <a href="#" class="nav-link">
                     <i class="nav-icon far fa-building"></i>
                     <p>
                         Компании
+                        <i class="fas fa-angle-left right"></i>
                     </p>
                 </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('employers.index') }}">
+                            <i class="far fa-circle nav-icon"></i>
+                            Список
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('employers.create') }}">
+                            <i class="far fa-circle nav-icon"></i>
+                            Добавить
+                        </a>
+                    </li>
+                </ul>
             </li>
-            <li class="nav-item">
-                <a href="{{ route('people.index') }}" class="nav-link">
+            <li class="nav-item"> {{-- Меню контактных лиц --}}
+                <a href="#" class="nav-link">
                     <i class="nav-icon far fa-address-book"></i>
                     <p>
                         Контактные лица
-                    </p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="nav-icon fas fa-user-secret"></i>
-                    <p>
-                        Администрирование
                         <i class="fas fa-angle-left right"></i>
                     </p>
-                </a><ul class="nav nav-treeview">
+                </a>
+                <ul class="nav nav-treeview">
                     <li class="nav-item">
-
+                        <a class="nav-link" href="{{ route('people.index') }}">
+                            <i class="far fa-circle nav-icon"></i>
+                            Список
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('people.create') }}">
+                            <i class="far fa-circle nav-icon"></i>
+                            Добавить
+                        </a>
                     </li>
                 </ul>
             </li>
