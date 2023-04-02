44d09f972c9b03a2741d6265701787a62daef242
Toy Rik <toyrik@gmail.com>
2023-04-02 18:18:17 +0300

    Переносим кнопку для создания контактных лиц в основную карточку списка

--- resources/views/people/index.blade.php
+++ resources/views/people/index.blade.php
@@ -34,12 +34,6 @@
                 </div>
             </div>
             <div class="card-body">
-
-                <a class="btn btn-primary btn-sm" href="{{ route('people.create') }}">
-                    <i class="fas fa-plus-circle">
-                    </i>
-                    Добавить
-                </a>
             </div>
             <!-- /.card-body -->
         </div>
@@ -49,12 +43,11 @@
                 <h3 class="card-title">Контактные лица</h3>
 
                 <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                        <i class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                        <i class="fas fa-times"></i>
-                    </button>
+                    <a class="btn btn-primary btn-sm" href="{{ route('people.create') }}">
+                        <i class="fas fa-plus-circle">
+                        </i>
+                        Добавить
+                    </a>
                 </div>
             </div>
             <div class="card-body p-0">
