6c0ed6687c0422f6f8e1ab7c53ab07fcc8a243c5
Toy Rik <toyrik@gmail.com>
2023-05-15 07:09:29 +0300

    Принудительный https для прода

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Cron\CronExpression;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFour();
+        if (env('APP_FORCE_HTTPS', false)) {
+            URL::forceSceme('https');
+        }
         Validator::extend('cron', function ($attribute, $value, $parameters, $validator) {
         return CronExpression::isValidExpression($value);
     });
