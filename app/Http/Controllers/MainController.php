e40279b18ab58fea970587f9e65bc6950ad199df
Toy Rik <toyrik@gmail.com>
2023-05-19 07:56:13 +0300

    FIX Отображение количества вакансий не относящихся к пользователю

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,8 +9,9 @@ class MainController extends Controller
 {
     public function index()
     {
-        $vacanciesDraftCount = Vacancy::whereStatus('draft')->count();
-        $vacanciesActiveCount = Vacancy::whereStatus('active')->count();
+        $user = user();
+        $vacanciesDraftCount = Vacancy::whereStatus('draft')->whereUserId($user->id)->count();
+        $vacanciesActiveCount = Vacancy::whereStatus('active')->whereUserId($user->id)->count();
         return view(
             'dashboard',
             compact(
