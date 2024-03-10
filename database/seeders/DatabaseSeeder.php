d09d419444f79a585e4730d7fde4e034fbae07c4
martakhimich <marta_himich@mail.ru>
2024-03-10 20:56:51 +0300

    Исправлены ошибки посева данных в базе

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             PersonSeeder::class,
             EmployerSeeder::class,
             VacancySeeder::class,
